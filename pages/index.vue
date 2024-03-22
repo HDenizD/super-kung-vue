d3ce22a3259ed204c9573a720654b9f4e3eb0517
HDenizD <denizderezzed@gmail.com>
2024-03-22 14:34:19 +0100

    Refactor index.vue component

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,34 @@
 <template>
   <div>
-    <section class="">
-      <div class="md-absolute-center flex flex-col items-center">
-        <h1
-          class="inline-block bg-gradient-to-r from-primary to-secondary bg-clip-text text-center font-brushking text-6xl leading-[8rem] tracking-widest text-transparent"
+    <section>
+      <Transition
+        enter-active-class="animate__lightSpeedInLeft"
+        leave-active-class="animate__lightSpeedOutLeft animate__faster"
+      >
+        <div
+          v-if="$route.fullPath === '/'"
+          class="animate__animated md-absolute-center flex flex-col items-center"
         >
-          Super <br />
-          Kung Vue
-        </h1>
-        <div class="mt-14 flex flex-col gap-10">
-          <KVBtn
-            is-retro
-            label="START"
-            @click="$router.push('/game/player-name')"
-          />
-          <KVBtn
-            is-retro
-            label="SETTINGS"
-            @click="$router.push('/settings')"
-          />
+          <h1
+            class="inline-block bg-gradient-to-r from-primary to-secondary bg-clip-text text-center font-brushking text-6xl leading-[8rem] tracking-widest text-transparent"
+          >
+            Super <br />
+            Kung Vue
+          </h1>
+          <div class="mt-14 flex flex-col gap-10">
+            <KVBtn
+              is-retro
+              label="START"
+              @click="$router.push('/game/player-name')"
+            />
+            <KVBtn
+              is-retro
+              label="SETTINGS"
+              @click="$router.push('/settings')"
+            />
+          </div>
         </div>
-      </div>
+      </Transition>
     </section>
   </div>
 </template>
