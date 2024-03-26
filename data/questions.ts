e8b55117257273ecaf496d40a0c858db9232694a
HDenizD <denizderezzed@gmail.com>
2024-03-26 11:17:28 +0100

    Add new questions and update existing questions in questions.ts

--- data/questions.ts
+++ data/questions.ts
@@ -22,53 +22,177 @@ export const questions: Stage[] = [
     ]
   },
   {
-    question: 'Lorem',
+    question: 'What is the main feature of Vue.js?',
     damageOnWrongAnswer: 20,
     options: [
-      { option: '1', isCorrect: true },
-      { option: '2', isCorrect: false },
-      { option: '3', isCorrect: false },
-      { option: '4', isCorrect: false }
+      {
+        option: 'Component-based architecture',
+        isCorrect: true
+      },
+      {
+        option: 'Server-side rendering',
+        isCorrect: false
+      },
+      { option: 'Static typing', isCorrect: false },
+      {
+        option: 'Native mobile app development',
+        isCorrect: false
+      }
     ]
   },
   {
-    question: 'Ipsum',
+    question: 'What does Vue.js use for rendering?',
     damageOnWrongAnswer: 20,
     options: [
-      { option: '1', isCorrect: false },
-      { option: '2', isCorrect: false },
-      { option: '3', isCorrect: true },
-      { option: '4', isCorrect: false }
+      {
+        option: 'Virtual DOM',
+        isCorrect: true
+      },
+      {
+        option: 'Shadow DOM',
+        isCorrect: false
+      },
+      { option: 'Real DOM', isCorrect: false },
+      {
+        option: 'HTML5 Canvas',
+        isCorrect: false
+      }
     ]
   },
   {
-    question: 'Dolor',
+    question:
+      'What lifecycle hook is called right after the instance is created?',
     damageOnWrongAnswer: 20,
     options: [
-      { option: '1', isCorrect: false },
-      { option: '2', isCorrect: false },
-      { option: '3', isCorrect: false },
-      { option: '4', isCorrect: true }
+      {
+        option: 'created',
+        isCorrect: true
+      },
+      {
+        option: 'mounted',
+        isCorrect: false
+      },
+      { option: 'updated', isCorrect: false },
+      {
+        option: 'destroyed',
+        isCorrect: false
+      }
     ]
   },
   {
-    question: 'Sit',
+    question: 'What is the purpose of the `v-for` directive in Vue.js?',
     damageOnWrongAnswer: 20,
     options: [
-      { option: '1', isCorrect: false },
-      { option: '2', isCorrect: true },
-      { option: '3', isCorrect: false },
-      { option: '4', isCorrect: false }
+      {
+        option: 'To render a list of items based on an array',
+        isCorrect: true
+      },
+      {
+        option: 'To loop over the properties of an object',
+        isCorrect: false
+      },
+      { option: 'To conditionally render an element', isCorrect: false },
+      {
+        option: 'To bind data to a form input field',
+        isCorrect: false
+      }
     ]
   },
   {
-    question: 'Amet',
+    question: 'What is computed property in Vue.js?',
     damageOnWrongAnswer: 20,
     options: [
-      { option: '1', isCorrect: false },
-      { option: '2', isCorrect: false },
-      { option: '3', isCorrect: false },
-      { option: '4', isCorrect: true }
+      {
+        option:
+          'A property whose value is calculated based on other data properties',
+        isCorrect: true
+      },
+      {
+        option: 'A property that is updated asynchronously',
+        isCorrect: false
+      },
+      { option: 'A property that can only be set once', isCorrect: false },
+      {
+        option: 'A property that triggers a method when accessed',
+        isCorrect: false
+      }
+    ]
+  },
+  {
+    question: 'Which directive is used to conditionally display an element?',
+    damageOnWrongAnswer: 20,
+    options: [
+      {
+        option: 'v-if',
+        isCorrect: true
+      },
+      {
+        option: 'v-show',
+        isCorrect: false
+      },
+      { option: 'v-bind', isCorrect: false },
+      {
+        option: 'v-model',
+        isCorrect: false
+      }
+    ]
+  },
+  {
+    question: 'What is the purpose of the `v-bind` directive in Vue.js?',
+    damageOnWrongAnswer: 20,
+    options: [
+      {
+        option: 'To dynamically bind an attribute to an expression',
+        isCorrect: true
+      },
+      {
+        option: 'To conditionally render an element',
+        isCorrect: false
+      },
+      { option: 'To iterate over a list', isCorrect: false },
+      {
+        option: 'To bind data to a form input field',
+        isCorrect: false
+      }
+    ]
+  },
+  {
+    question: 'What is the purpose of the `v-on` directive in Vue.js?',
+    damageOnWrongAnswer: 20,
+    options: [
+      {
+        option: 'To attach event listeners to DOM elements',
+        isCorrect: true
+      },
+      {
+        option: 'To dynamically bind an attribute to an expression',
+        isCorrect: false
+      },
+      { option: 'To conditionally render an element', isCorrect: false },
+      {
+        option: 'To bind data to a form input field',
+        isCorrect: false
+      }
+    ]
+  },
+  {
+    question:
+      'Which Vue.js feature allows for code reuse and separation of concerns?',
+    damageOnWrongAnswer: 20,
+    options: [
+      {
+        option: 'Components',
+        isCorrect: true
+      },
+      {
+        option: 'Directives',
+        isCorrect: false
+      },
+      { option: 'Mixins', isCorrect: false },
+      {
+        option: 'Filters',
+        isCorrect: false
+      }
     ]
   }
 ]
