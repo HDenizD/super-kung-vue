7ff0ed787be63e0630c347a8ba3439de2bf7c972
HDenizD <denizderezzed@gmail.com>
2024-04-08 13:02:13 +0200

    Add new tests for game reset and try again functionality

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import { baseUrl, playerNameUrl } from './urlRoutes'
+import { baseUrl, playerNameUrl, stageHubUrl } from './urlRoutes'
 
 test('App is running and page title says "Super Kung Vue"', async ({
   page
@@ -29,4 +29,31 @@ test('navigate back to index on logo click', async ({ page }) => {
   await page.getByRole('heading', { name: 'Super Kung Vue' }).click()
   await page.waitForURL(baseUrl)
   expect(page.url()).toBe(baseUrl)
+})
+
+test('reset game', async ({ page }) => {
+  await page.goto(baseUrl)
+  const startGameBtn = page.getByTestId('start-game')
+  await startGameBtn.click()
+  await page.getByLabel('PLAYER NAME').click()
+  await page.getByLabel('PLAYER NAME').fill('Test User')
+  await page.getByRole('button', { name: 'SUBMIT' }).click()
+  await page.getByRole('button', { name: 'Reset Game' }).click()
+  await page.getByRole('button', { name: "Nah, I'm Good" }).click()
+  await page.waitForURL(baseUrl)
+  expect(page.url()).toBe(baseUrl)
+  await expect(startGameBtn).toBeVisible()
+})
+
+test('try again', async ({ page }) => {
+  await page.goto(baseUrl)
+  const startGameBtn = page.getByTestId('start-game')
+  await startGameBtn.click()
+  await page.getByLabel('PLAYER NAME').click()
+  await page.getByLabel('PLAYER NAME').fill('Test User')
+  await page.getByRole('button', { name: 'SUBMIT' }).click()
+  await page.getByRole('button', { name: 'Reset Game' }).click()
+  await page.getByRole('button', { name: 'Try Again' }).click()
+  await page.waitForURL(stageHubUrl)
+  expect(page.url()).toBe(stageHubUrl)
 })
