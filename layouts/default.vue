697922069378e04cd5669f9dfc386421357250d3
HDenizD <denizderezzed@gmail.com>
2024-04-17 12:28:25 +0200

    Co-authored-by: Pascal <pascal-koop@users.noreply.github.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="mx-auto max-w-7xl">
     <ColorScheme tag="span">
-      <div class="flex min-h-screen flex-col">
+      <div class="relative flex min-h-full flex-col">
         <header
-          class="dark:border-tertiary sticky top-0 z-50 mb-2 flex items-center bg-light bg-opacity-95 p-2 pt-1 dark:bg-dark dark:bg-opacity-95"
+          class="dark:border-tertiary sticky left-0 top-0 z-50 flex h-14 items-center bg-light bg-opacity-95 p-2 dark:bg-dark dark:bg-opacity-95"
         >
           <TheNavigation />
         </header>
-        <main class="flex-grow px-2 pb-20 pt-8 md:px-4">
+        <main class="flex-grow px-2 md:px-4">
           <slot />
         </main>
         <footer
-          class="dark:border-tertiary fixed bottom-0 right-0 flex h-auto w-full items-center justify-between overflow-hidden bg-light bg-opacity-95 p-2 shadow-lg dark:bg-dark dark:bg-opacity-95"
+          class="dark:border-tertiary sticky bottom-0 left-0 z-50 flex h-14 w-full items-center justify-between overflow-hidden bg-light bg-opacity-95 p-2 shadow-lg dark:bg-dark dark:bg-opacity-95"
         >
           <TheFooter />
           <PlayerHealthBar
