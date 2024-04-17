c2f826ad62030a411c949f82b8d151c42c94aa36
HDenizD <denizderezzed@gmail.com>
2024-04-17 13:30:38 +0200

    Update default.vue layout styling and add color mode toggle functionality

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,26 @@
 <template>
   <ColorScheme tag="span">
-    <div class="mx-auto max-w-7xl">
-      <div class="relative flex min-h-screen flex-col">
-        <header
-          class="dark:border-tertiary sticky left-0 top-0 z-50 flex h-14 items-center bg-light bg-opacity-95 p-2 dark:bg-dark dark:bg-opacity-95"
-        >
-          <TheNavigation />
-        </header>
-        <main class="flex-grow px-2 md:px-4">
-          <slot />
-        </main>
-        <footer
-          class="dark:border-tertiary sticky bottom-0 left-0 z-50 flex h-14 w-full items-center justify-between overflow-hidden bg-light bg-opacity-95 p-2 shadow-lg dark:bg-dark dark:bg-opacity-95"
-        >
-          <TheFooter />
-          <PlayerHealthBar
-            v-if="isGameInProgress"
-            class="z-50 md:hidden"
-            :player-health="playerHealth"
-            is-small
-            is-full-width
-          />
-        </footer>
-      </div>
+    <div class="relative mx-auto flex min-h-screen max-w-7xl flex-col">
+      <header
+        class="dark:border-tertiary sticky left-0 top-0 z-50 flex h-14 items-center bg-light bg-opacity-95 p-2 dark:bg-dark dark:bg-opacity-95"
+      >
+        <TheNavigation />
+      </header>
+      <main class="flex-grow px-2 py-10 md:px-4">
+        <slot />
+      </main>
+      <footer
+        class="dark:border-tertiary sticky bottom-0 left-0 z-50 flex h-14 w-full items-center justify-between overflow-hidden bg-light bg-opacity-95 p-2 shadow-lg dark:bg-dark dark:bg-opacity-95"
+      >
+        <TheFooter />
+        <PlayerHealthBar
+          v-if="isGameInProgress"
+          class="z-50 md:hidden"
+          :player-health="playerHealth"
+          is-small
+          is-full-width
+        />
+      </footer>
     </div>
   </ColorScheme>
 </template>
