11ab31384c2ffe9a71256258011ba111f4e7c4f9
Deniz <denizderezzed@gmail.com>
2024-05-11 20:59:59 +0200

    Update default.vue layout styling (#5)

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,15 @@
   <ColorScheme tag="span">
     <div class="relative mx-auto flex min-h-screen max-w-7xl flex-col">
       <header
-        class="dark:border-tertiary sticky left-0 top-0 z-50 flex h-14 items-center bg-light bg-opacity-95 p-2 dark:bg-dark dark:bg-opacity-95"
+        class="dark:border-tertiary sticky left-0 top-0 z-50 flex h-16 items-center bg-light bg-opacity-95 p-4 dark:bg-dark dark:bg-opacity-95"
       >
         <TheNavigation />
       </header>
-      <main class="h-0 flex-grow overflow-y-auto px-2 py-9 md:px-4">
+      <main class="mx-4 flex-1 py-8">
         <slot />
       </main>
       <footer
-        class="dark:border-tertiary sticky bottom-0 left-0 z-50 flex h-14 w-full items-center justify-between overflow-hidden bg-light bg-opacity-95 p-2 shadow-lg dark:bg-dark dark:bg-opacity-95"
+        class="dark:border-tertiary sticky bottom-0 left-0 z-50 flex h-16 w-full items-center justify-between overflow-hidden bg-light bg-opacity-95 p-4 shadow-lg dark:bg-dark dark:bg-opacity-95"
       >
         <TheFooter />
         <PlayerHealthBar
